c08aa4f84d0046421e004b7c5dc95516581b4313
Jin Sung Yoo <ujinsung@gmail.com>
2011-04-26 21:00:59 -0400

    fix memory warning problem on Quiz Menu section.

---
+++ Classes/MoreDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  MoreDetailViewController.h
+//  MMBB
+//
+//  Created by Jin Sung Yoo on 4/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MoreDetailViewController : UIViewController {
+    
+}
+
+@end
---
+++ Classes/MoreViewsTableController.h
@@ -0,0 +1,16 @@
+//
+//  MoreViewsTableController.h
+//  MMBB
+//
+//  Created by Jin Sung Yoo on 4/18/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MoreViewsTableController : UITableViewController {
+    
+}
+
+@end
